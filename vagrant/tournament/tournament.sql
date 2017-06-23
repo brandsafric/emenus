2adb0756357c3fc36c0b824856ff359419658487
Phillip Stafford <phillip@philliprstafford.com>
2017-06-23 17:14:46 -0400

    Create Database. Create table for players. Create table for matches.

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,7 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
-
+create database tournament;
+\c tournament
+create table players (id serial, name text);
+create table matches (match_id serial, player_one text, player_two text, winner text);
