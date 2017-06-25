7e1908afdab92b3f357669e29b8b229029adbd0b
Phillip Stafford <phillip@philliprstafford.com>
2017-06-25 13:33:21 -0400

    Add drop database IF EXISTS command to top of file.

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,6 +6,8 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+drop database IF EXISTS tournament;
+
 create database tournament;
 \c tournament
 create table players (id serial, name text);
