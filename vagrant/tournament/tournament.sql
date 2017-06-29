f649b390bf9bf4d2ad1a99713b51af1f736be5f9
Phillip Stafford <phillip@philliprstafford.com>
2017-06-29 16:37:59 -0400

    Change table for matches so that each match has two rows.

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -11,7 +11,7 @@ DROP DATABASE IF EXISTS tournament;
 CREATE DATABASE tournament;
 \c tournament
 CREATE TABLE players (id serial, name text);
-CREATE TABLE matches (match_id serial, player_one text, player_two text, winner text, winner_id integer);
+CREATE TABLE matches (match integer, player text, winner text, winner_id integer);
 CREATE VIEW match_winners AS SELECT match_id, winner FROM matches GROUP BY match_id, winner ORDER BY match_id ASC;
 CREATE VIEW player_scores AS SELECT winner AS player, count(*) AS wins FROM match_winners GROUP BY winner ORDER BY wins DESC;
 
