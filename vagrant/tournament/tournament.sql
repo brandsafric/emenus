9a2963d13e9e243b71ee0827e56859a9781d3926
Phillip Stafford <phillip@philliprstafford.com>
2017-07-01 16:55:06 -0400

    Refactor player_standings to show all players.

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -12,50 +12,44 @@ CREATE DATABASE tournament;
 \c tournament
 CREATE TABLE players (id serial, name text);
 CREATE TABLE matches (match serial, loser integer, winner integer);
-CREATE VIEW match_winners AS SELECT match, winner FROM matches GROUP BY match, winner ORDER BY match ASC;
---CREATE VIEW player_standings AS SELECT sum1.player, sum1.name, sum1.match_wins
---FROM (
---SELECT players.id AS player, players.name as name, COUNT(match_winners.winner) AS match_wins FROM players LEFT JOIN match_winners ON players.id = match_winners.winner
---GROUP BY players.id, players.name
---) sum1
---RIGHT JOIN players
---ON players.name = sum1.name ORDER BY sum1.match_wins DESC, sum1.player;
+--3 Column Table player | name | match_wins
+CREATE VIEW match_winners AS
+SELECT match, winner FROM matches GROUP BY match, winner ORDER BY match ASC;
 
-
---SELECT DISTINCT sum1.player, sum1.name, sum1.match_wins
---FROM (
---SELECT players.id AS player, players.name as name, COUNT(match_winners.winner) AS match_wins FROM players LEFT JOIN match_winners ON players.id = match_winners.winner
---GROUP BY players.id, players.name
---) sum1, (
---SELECT players.id AS player, players.name as name, COUNT(matches.loser) AS match_losers FROM players LEFT JOIN matches ON players.id = matches.winner
---GROUP BY players.id, players.name
---) sum2
---ORDER BY sum1.match_wins DESC, sum1.player;
-
+--4 Columns player | name | match_wins | matches_played
 CREATE VIEW player_standings AS
-SELECT DISTINCT sum1.player, sum1.name, sum1.match_wins, sum3.matches_played
+SELECT sum1.player, sum1.name, sum1.match_wins, sum4.matches_played
 FROM (
-SELECT players.id AS player, players.name as name, COUNT(match_winners.winner) AS match_wins FROM players LEFT JOIN match_winners ON players.id = match_winners.winner
+SELECT players.id AS player, players.name as name, COUNT(match_winners.winner) AS match_wins
+FROM players
+LEFT JOIN match_winners ON
+players.id = match_winners.winner
 GROUP BY players.id, players.name
-) sum1, (
-SELECT players.id AS player, players.name as name, COUNT(matches.loser) AS match_losers FROM players LEFT JOIN matches ON players.id = matches.winner
+) sum1,
+(
+SELECT players.id AS player, players.name as name, COUNT(matches.loser) AS match_losers
+FROM players
+LEFT JOIN matches ON
+players.id = matches.winner
 GROUP BY players.id, players.name
 ) sum2,
 (
-SELECT player, COUNT(*) matches_played
-FROM
-(
-  SELECT loser as player
-  FROM matches
-  UNION ALL
-  SELECT winner
-  FROM matches
-) m
-GROUP BY player
-ORDER BY matches_played DESC
+SELECT players.id, COUNT(*) matches_played
+FROM (
+SELECT loser as player
+FROM matches
+UNION ALL
+SELECT winner
+FROM matches
 ) sum3
-GROUP BY sum1.player, sum1.name, sum1.match_wins, sum3.matches_played
+RIGHT join players on
+players.id = sum3.player
+GROUP BY players.id, sum3.player
+ORDER BY matches_played DESC
+) sum4
+GROUP BY sum1.player, sum1.name, sum1.match_wins, sum4.matches_played
 ORDER BY sum1.match_wins DESC, sum1.player;
+
 
 
 
