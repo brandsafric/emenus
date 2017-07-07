4f5673dacf8e3829cffdb5033427bc9681672189
Phillip Stafford <phillip@philliprstafford.com>
2017-07-07 17:07:21 -0400

    Complete puppy_query.py

---
+++ vagrant/restaurant/lesson_examples.py
@@ -0,0 +1,65 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from database_setup import Base, Restaurant, MenuItem
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind = engine)
+session = DBSession()
+myFirstRestaurant = Restaurant(name = "Pizza Palace")
+session.add(myFirstRestaurant)
+session.commit()
+session.query(Restaurant).all()
+cheesePizza = MenuItem(name = "Cheese Pizza",
+                       description = "Made with all natural ingredients and fresh mozzarella",
+                       course = "Entree",
+                       price = "$8.99",
+                       restaurant = myFirstRestaurant)
+session.add(cheesePizza)
+session.commit()
+session.query(MenuItem).all()
+
+firstResult = session.query(Restaurant).all()
+firstResult = session.query(Restaurant).first()
+
+firstResult.name
+
+items = session.query(MenuItem).all()
+for item in items:
+    print item.name
+
+veggieBurgers = session.query(MenuItem).filter_by(name = 'Veggie Burger')
+for veggieBurger in veggieBurgers:
+    print veggieBurger.id
+    print veggieBurger.price
+    print veggieBurger.restaurant.name
+    print "\n"
+
+UrbanVeggieBurger = session.query(MenuItem).filter_by(id = 8).one()
+print UrbanVeggieBurger.price
+UrbanVeggieBurger.price = '$2.99'
+session.add(UrbanVeggieBurger)
+session.commit()
+
+for veggieBurger in veggieBurgers:
+    if veggieBurger.price != '$2.99':
+        veggieBurger.price = '$2.99'
+        session.add(veggieBurger)
+        session.commit()
+
+veggieBurgers = session.query(MenuItem).filter_by(name = 'Veggie Burger')
+for veggieBurger in veggieBurgers:
+    print veggieBurger.id
+    print veggieBurger.price
+    print veggieBurger.restaurant.name
+    print "\n"
+
+spinach = session.query(MenuItem).filter_by(name = 'Spinach Ice Cream').one()
+print spinach.restaurant.name
+
+session.delete(spinach)
+session.commit()
+spinach = session.query(MenuItem).filter_by(name = 'Spinach Ice Cream').one()
+
+
