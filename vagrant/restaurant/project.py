afcf1e644654ab23eed473b10605367901dc37d7
Phillip Stafford <phillip@philliprstafford.com>
2017-07-21 12:46:16 -0400

    Commented out unnecessary code.

--- vagrant/restaurant/project.py
+++ vagrant/restaurant/project.py
@@ -27,33 +27,33 @@ def restaurantMenu(restaurant_id):
     return output
 
 # Task 1: Create route for newMenuItem function here
-@app.route('/restaurants/new/<int:restaurant_id>/')
+@app.route('/restaurants/<int:restaurant_id>/new')
 def newMenuItem(restaurant_id):
-    try:
-        restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
-        output = ''
-        output += restaurant.name
-        output += ' already exists.  Choose another index.'
-    except:
-        output = ''
-        output += 'Enter the name of the new restaurant below.'
-    return output
+    # try:
+    #     restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
+    #     output = ''
+    #     output += restaurant.name
+    #     output += ' already exists.  Choose another index.'
+    # except:
+    #     output = ''
+    #     output += 'Enter the name of the new restaurant below.'
+    # return output
     return "page to create a new menu item.  Task 1 complete!"
 
 # Task 2: Create route for editMenuItem function here
 
 @app.route('/restaurants/edit/<int:restaurant_id>/<int:menu_id>')
 def editMenuItem(restaurant_id, menu_id):
-    restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
-    item = session.query(MenuItem).filter_by(id = menu_id).one()
-    output = ''
-    output += item.name
-    output += '<br>'
-    output += item.price
-    output += "<br>"
-    output += item.description
-    output += "<br><br>"
-    return output
+    # restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
+    # item = session.query(MenuItem).filter_by(id = menu_id).one()
+    # output = ''
+    # output += item.name
+    # output += '<br>'
+    # output += item.price
+    # output += "<br>"
+    # output += item.description
+    # output += "<br><br>"
+    # return output
 
 
     return "page to edit a menu item.  Take 2 complete!"
@@ -61,16 +61,16 @@ def editMenuItem(restaurant_id, menu_id):
 # Task 3: Create a route for deleteMenuItem function here
 @app.route('/restaurants/delete/<int:restaurant_id>/<int:menu_id>')
 def deleteMenuItem(restaurant_id, menu_id):
-    restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
-    item = session.query(MenuItem).filter_by(id = menu_id).one()
-    output = ''
-    output += item.name
-    output += '<br>'
-    output += item.price
-    output += "<br>"
-    output += item.description
-    output += "<br><br>"
-    return output
+    # restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
+    # item = session.query(MenuItem).filter_by(id = menu_id).one()
+    # output = ''
+    # output += item.name
+    # output += '<br>'
+    # output += item.price
+    # output += "<br>"
+    # output += item.description
+    # output += "<br><br>"
+    # return output
 
     return "page to delete a menu item.  Task 3 complete!"
 
