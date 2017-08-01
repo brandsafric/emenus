248d927dde07e7e6036257678c6ed41d938982a4
Phillip Stafford <phillip@philliprstafford.com>
2017-08-01 11:18:07 -0400

    Add code for DB connection.  Change showRestaurants return statement to return the session query results.

--- vagrant/restaurant/FinalProject.py
+++ vagrant/restaurant/FinalProject.py
@@ -1,6 +1,16 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
 
 app = Flask(__name__)
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
 
 #Fake Restaurants
 restaurant = {'name': 'The CRUDdy Crab', 'id': '1'}
@@ -16,8 +26,10 @@ item =  {'name':'Cheese Pizza','description':'made with fresh cheese','price':'$
 @app.route('/restaurants/')
 def showRestaurants():
     # return 'This page will show all my restaurants.'
-    return render_template('restaurant.html', restaurants=restaurants, items=items)
-
+    # return render_template('restaurant.html', restaurants=restaurants, items=items)
+    restaurants = session.query(Restaurant).all()
+    return render_template(
+        'restaurant.html', restaurants=restaurants)
 @app.route('/restaurants/new')
 def newRestaurant():
     # return "This page will be for making a new restaurant."
