2e7f59d5cec3ba4e72c744fd8780af5c7caa5bdc
grimal <phillip@philliprstafford.com>
2017-08-10 11:34:43 -0400

    Add toggle functionality to each menu category. Add app.js file with jquery toggleClass for chevron up or down.  Collapse section/expand section on header click.

--- vagrant/restaurant/static/app.js
+++ vagrant/restaurant/static/app.js
@@ -1,16 +1,47 @@
-(function () {
-    function collapseNavbar() {
-        if ($(".navbar").offset().top > 50) {
-            $(".navbar-fixed-top").addClass("top-nav-collapse");
-        } else {
-            $(".navbar-fixed-top").removeClass("top-nav-collapse");
-        }
-    }
-    $(window).scroll(collapseNavbar);
-    $(document).ready(collapseNavbar);
-    $('.navbar-collapse ul li a').click(function () {
-        if ($(this).attr('class') !== 'dropdown-toggle active' && $(this).attr('class') !== 'dropdown-toggle') {
-            $('.navbar-toggle:visible').click();
-        }
-    });
-})();
+$(function () {
+        var bindEvents = function() {
+            $("#appetizer-section").click(function () {
+                $("#appetizer-row").slideToggle();
+                if ($("#appetizer-toggle").hasClass("fa-chevron-up")) {
+                    $("#appetizer-toggle").toggleClass("fa-chevron-up");
+                    $("#appetizer-toggle").toggleClass("fa-chevron-down");
+                } else {
+                    $("#appetizer-toggle").toggleClass("fa-chevron-down");
+                    $("#appetizer-toggle").toggleClass("fa-chevron-up");
+                }
+            });
+            $("#entree-section").click(function () {
+                $("#entree-row").slideToggle();
+                if ($("#entree-toggle").hasClass("fa-chevron-up")) {
+                    $("#entree-toggle").toggleClass("fa-chevron-up");
+                    $("#entree-toggle").toggleClass("fa-chevron-down");
+                } else {
+                    $("#entree-toggle").toggleClass("fa-chevron-down");
+                    $("#entree-toggle").toggleClass("fa-chevron-up");
+                }
+            });
+            $("#dessert-section").click(function () {
+                $("#dessert-row").slideToggle();
+                if ($("#dessert-toggle").hasClass("fa-chevron-up")) {
+                    $("#dessert-toggle").toggleClass("fa-chevron-up");
+                    $("#dessert-toggle").toggleClass("fa-chevron-down");
+                } else {
+                    $("#dessert-toggle").toggleClass("fa-chevron-down");
+                    $("#dessert-toggle").toggleClass("fa-chevron-up");
+                }
+            });
+            $("#beverage-section").click(function () {
+                $("#beverage-row").slideToggle();
+                if ($("#beverage-toggle").hasClass("fa-chevron-up")) {
+                    $("#beverage-toggle").toggleClass("fa-chevron-up");
+                    $("#beverage-toggle").toggleClass("fa-chevron-down");
+                } else {
+                    $("#beverage-toggle").toggleClass("fa-chevron-down");
+                    $("#beverage-toggle").toggleClass("fa-chevron-up");
+                }
+            });
+        };
+
+        bindEvents();
+
+});
