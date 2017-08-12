4f598401bd2f98e6b8662978d1c3cf44d11acad0
grimal <phillip@philliprstafford.com>
2017-08-12 17:03:46 -0400

    Add method for google disconnect. Add stored_credentials to store credentials.

--- vagrant/restaurant/FinalProject.py
+++ vagrant/restaurant/FinalProject.py
@@ -203,8 +203,9 @@ def gconnect():
         return response
 
     stored_access_token = login_session.get('access_token')
+    stored_credentials = login_session.get('credentials')
     stored_gplus_id = login_session.get('gplus_id')
-    if stored_access_token is not None and gplus_id == stored_gplus_id:
+    if stored_credentials is not None and gplus_id == stored_gplus_id:
         response = make_response(json.dumps('Current user is already connected.'),
                                  200)
         response.headers['Content-Type'] = 'application/json'
@@ -236,9 +237,43 @@ def gconnect():
     flash("you are now loggin in as {0}".format(login_session['username']))
     return output
 
+#DISCONNECT - Revoke a current user's token and reset their login session.
+@app.route("/gdisconnect")
+def gdisconnect():
+    # Only disconnect a connected user.
+    access_token = login_session.get('access_token')
+    if access_token is None:
+        print "Access Token is none"
+        response = make_response(json.dumps("Current user not connected."), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    # Execute HTTP GET request to revoke current token.
+    print 'In gdisconnect access token is {0}'.format(access_token)
+    print 'User name is: '
+    print login_session['username']
+    url = 'https://accounts.google.com/o/oauth2/revoke?token={0}'.format(access_token)
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[0]
+    print 'result is '
+    print result
+
+    if result['status'] == '200':
+        # Reset the user's session
+        del login_session['access_token']
+        del login_session['gplus_id']
+        del login_session['username']
+        del login_session['email']
+        del login_session['picture']
+
+        response = make_response(json.dumps("Successfully disconnected."), 200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    else:
+        response = make_response(json.dumps('Failed to revoke token for given user.', 400))
+        response.headers['Content-Type'] = 'application/json'
+        return response
 
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
     app.debug = True
     app.run(host='0.0.0.0', port=5000)
-
