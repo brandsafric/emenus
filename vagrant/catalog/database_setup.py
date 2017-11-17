2217643229545818a0f041b771c3335d64fecbc6
grimal <phillip@philliprstafford.com>
2017-11-17 14:14:18 -0500

    Change image_url to picture.

--- vagrant/catalog/database_setup.py
+++ vagrant/catalog/database_setup.py
@@ -20,7 +20,7 @@ class Restaurant(Base):
 
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
-    image_url = Column(String(250))
+    picture = Column(String(250))
     user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship(User)
 
