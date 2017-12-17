6ffd55aa2597b9e510b9ea527cc4ae19dc879f5c
grimal <phillip@philliprstafford.com>
2017-12-17 09:25:37 -0500

    When clicking selected image, do not toggle selection.  This ensures that always one picture is selected.

--- vagrant/catalog/static/app.js
+++ vagrant/catalog/static/app.js
@@ -50,26 +50,37 @@ $(function () {
                 // $(event.target).hasClass('textbox')
                 if ($(event.target).hasClass('img_thumbnail')) {
                     console.log('it is thumbnail');
-                    $(event.target).toggleClass('selected');
-                    $(event.target).siblings(".selected").toggleClass("selected");
-                      // var myUL = $(this).siblings(".selected").toggleClass("showme");
-                    var src = $(event.target).firstElementChild;
-                    console.log($(event.target));
-                   console.log($(event.target)[0]);
-                    console.log(src);
-                    console.log(src.eq(0).attr('data-imgpath'));
-                    // $('#target').child().val('');
+                    if ($(event.target).hasClass('selected')) {
+                        // do nothing
+                    } else {
+                            $(event.target).toggleClass('selected');
+                            $(event.target).siblings(".selected").toggleClass("selected");
+                            // var myUL = $(this).siblings(".selected").toggleClass("showme");
+                            var src = $(event.target).firstElementChild;
+                            // console.log($(event.target));
+                           // console.log($(event.target)[0]);
+                            // console.log(src);
+                            // console.log(src.eq(0).attr('data-imgpath'));
+                            var path = $(event.target).eq(0).attr('data-imgpath');
+                            $('#target').child().val('');
+                    }
+
                 }
                 else {
                     console.log('it is image');
                     console.log($(event.target));
-                    $(event.target).parent().toggleClass('selected');
-                    // $(this)[0].addClass('yellow_border');
-                    $(event.target).parent().siblings(".selected").toggleClass("selected");
-                    var src = $(event.target)[0].src;
-                    console.log(src);
-                    var path = $(event.target).eq(0).attr('data-imgpath');
-                    $('#target').val(path);
+                    if ($(event.target).parent().hasClass('selected')) {
+                        // do nothing
+                    } else {
+                        $(event.target).parent().toggleClass('selected');
+                        // $(this)[0].addClass('yellow_border');
+                        $(event.target).parent().siblings(".selected").toggleClass("selected");
+                        // var src = $(event.target)[0].src;
+                        // console.log(src);
+                        var path = $(event.target).eq(0).attr('data-imgpath');
+                        $('#target').val(path);
+                        }
+
 
                 }
                 // e.target.addClass('yellow_border');
