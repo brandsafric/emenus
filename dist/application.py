c9a9e225a1d9423274edcb91a65eff3033bb0a17
Phillip Stafford <phillip@philliprstafford.com>
2018-02-11 18:38:14 -0500

    Include APP_PAth in gconnect location of client_secrets.

--- dist/application.py
+++ dist/application.py
@@ -58,7 +58,7 @@ def gconnect():
 
     try:
         # Upgrade the authorization code into a credential object
-        oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
+        oauth_flow = flow_from_clientsecrets(APP_PATH + 'client_secrets.json', scope='')
         oauth_flow.redirect_uri = 'postmessage'
         credentials = oauth_flow.step2_exchange(code)
     except FlowExchangeError:
